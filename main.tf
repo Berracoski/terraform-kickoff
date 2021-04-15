9373cbb1981cde20c74f9bd86610c1f1a2b68a71
Pablo Linares <jorgepablolinares@gmail.com>
2021-04-14 22:40:28 -0300

    Terraforrm

--- main.tf
+++ main.tf
@@ -5,9 +5,10 @@ provider "aws" {
 resource "aws_instance" "despliegue-terraform-ec2" {
   ami		= "ami-06e2b3882a1e987b7"
   instance_type = "t2.micro"
-  key_name	= "mauricio-key"
+  key_name	= "ssh-aws"
+  vpc_security_group_ids = ["sg-0c1342408a815dcec"]
   tags = {
-    Name	= "Instance_test"
+    Name	= "Instance_test_terraform"
     terraform   = "True"
 
   }
